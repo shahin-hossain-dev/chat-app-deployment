4ae3337b8cae1e221121ea2aa0461507cbfda43f
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2025-01-25 22:15:15 +0600

    frontend: add: signup value get functionality

--- frontend/src/pages/signup/GenderCheckBox.jsx
+++ frontend/src/pages/signup/GenderCheckBox.jsx
@@ -1,16 +1,26 @@
-const GenderCheckBox = () => {
+const GenderCheckBox = ({ onCheckBoxChange, selectedGender }) => {
   return (
     <div className="flex gap-3">
       <div className="form-control">
         <label className="label cursor-pointer">
           <span className="label-text text-black">Male</span>
-          <input type="checkbox" className="checkbox" />
+          <input
+            type="checkbox"
+            className="checkbox"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckBoxChange("male")}
+          />
         </label>
       </div>
       <div className="form-control">
         <label className="label cursor-pointer">
           <span className="label-text text-black">Female</span>
-          <input type="checkbox" className="checkbox" />
+          <input
+            type="checkbox"
+            className="checkbox"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckBoxChange("female")}
+          />
         </label>
       </div>
     </div>
--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,22 +1,42 @@
 import { Link } from "react-router-dom";
 import GenderCheckBox from "./GenderCheckBox";
+import { useState } from "react";
 
 const Signup = () => {
+  const [inputs, setInputs] = useState({
+    fullname: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+
+  const onCheckBoxChange = (gender) => {
+    setInputs({ ...inputs, gender });
+  };
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
   return (
     <div>
       <div className="flex items-center justify-center flex-col max-w-96 mx-auto min-h-screen">
         <div className="h-full w-full p-4 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm border border-gray-100">
           <h2 className="text-3xl font-semibold text-center">Sign Up</h2>
-          <form className="card-body !px-3">
+          <form onSubmit={handleSignup} className="card-body !px-3">
             <div className="form-control">
               <label className="label">
                 <span className="label-text text-black">Full Name</span>
               </label>
               <input
                 type="text"
-                name="fullname"
                 placeholder="Enter Fullname "
                 className="input input-bordered w-full"
+                value={inputs.fullname}
+                onChange={(e) =>
+                  setInputs({ ...inputs, fullname: e.target.value })
+                }
                 required
               />
             </div>
@@ -28,6 +48,10 @@ const Signup = () => {
                 type="text"
                 placeholder="Enter username"
                 className="input input-bordered w-full"
+                value={inputs.username}
+                onChange={(e) =>
+                  setInputs({ ...inputs, username: e.target.value })
+                }
                 required
               />
             </div>
@@ -39,6 +63,10 @@ const Signup = () => {
                 type="password"
                 placeholder="Enter password"
                 className="input input-bordered w-full"
+                value={inputs.password}
+                onChange={(e) =>
+                  setInputs({ ...inputs, password: e.target.value })
+                }
                 required
               />
             </div>
@@ -50,10 +78,17 @@ const Signup = () => {
                 type="password"
                 placeholder="Enter confirm password"
                 className="input input-bordered w-full"
+                value={inputs.confirmPassword}
+                onChange={(e) =>
+                  setInputs({ ...inputs, confirmPassword: e.target.value })
+                }
                 required
               />
             </div>
-            <GenderCheckBox />
+            <GenderCheckBox
+              onCheckBoxChange={onCheckBoxChange}
+              selectedGender={inputs.gender}
+            />
             <div className="form-control mt-6">
               <button type="submit" className="btn btn-primary w-full">
                 Signup
