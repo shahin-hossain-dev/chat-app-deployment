dee80327a59fdb644566bcb0b9a137bb1966af4a
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2025-02-10 22:23:19 +0600

    frontend: add: message notification sound

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,14 +1,16 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
-
+import notification from "../../public/notification.mp3";
 const useListenMessages = () => {
   const { socket } = useSocketContext();
   const { messages, setMessages } = useConversation();
 
   useEffect(() => {
+    //listen socket message directly from server
     socket?.on("newMessage", (newMessage) => {
-      console.log(newMessage);
+      const sound = new Audio(notification);
+      sound.play();
       setMessages([...messages, newMessage]);
     });
     return () => {
